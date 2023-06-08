94de32d8ca1f29e9719e9eff9e96271ea22d39d5
asbjørn <qw41ha@student.aau.dk>
2023-06-08 10:44:05 +0200

    ros2 virker

---
+++ mini_ws/src/tcp_server_xml/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'tcp_server_xml'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='klyx',
+    maintainer_email='qw41ha@student.aau.dk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'server_node = tcp_server_xml.server_node:main'
+        ],
+    },
+)
